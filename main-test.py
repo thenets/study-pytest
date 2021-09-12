2322dad53e6bd0e5a122666d215c4d3273fc15b6
Luiz Costa <luiz@thenets.org>
2021-09-12 03:06:56 +0000

    Add run-test.py

--- main-test.py
+++
@@ -1,5 +0,0 @@
-if __name__ == "__main__":
-    print("This program is being run by itself")
-    import pytest
-
-    pytest.main(["-s", "."])
---
+++ run-test.py
@@ -0,0 +1,6 @@
+import pytest
+
+if __name__ == "__main__":
+    print("Starting tests...")
+
+    pytest.main(["-s", "."])
